3a6ed7ad5cf19ada63d96ae11add0496ed33cf70
yah01 <kagaminehuan@live.com>
2020-03-09 02:59:02 +0800

    添加调试窗口

--- DebugWindow.xaml.cs
+++ DebugWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,22 +8,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CipherBreaker
 {
 	/// <summary>
-	/// Interaction logic for MainWindow.xaml
+	/// DebugWindow.xaml 的交互逻辑
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class DebugWindow : Window
 	{
-		public MainWindow()
+		public DebugWindow()
 		{
 			InitializeComponent();
 
-			DebugWindow debugWindow = new DebugWindow();
-			debugWindow.Show();
+			Caesar caesar = new Caesar();
+			caesar.Plain = "hello";
+
+			DebugInfo.Content += "\n" + caesar.Plain;
 		}
 	}
 }
