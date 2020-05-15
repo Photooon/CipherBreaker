4f2bdc1d30c13a004a3d5ad477319dae5a6cd154
yah01 <kagaminehuan@gmail.com>
2020-05-15 18:41:03 +0800

    Create SymmetricScheme.cs

---
+++ SymmetricScheme.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace CipherBreaker
+{
+    abstract class SymmetricScheme : Scheme
+    {
+        protected string key;
+        public SymmetricScheme(string plain = null, string cipher = null, string key = null) : base(plain, cipher)
+        {
+            this.key = key;
+        }
+        public virtual string Key { get => key; set => key = value; }
+        protected abstract bool keyIsValid(string key = null);
+        public override abstract bool Break(string cipher = null);
+
+        public override abstract bool Decode(string cipher = null, string decodeKey = null);
+
+        public override abstract bool Encode(string plain = null, string encodeKey = null);
+
+        public override abstract bool Load(string fileName);
+
+        public override abstract bool Save(string fileName);
+
+        public override abstract string ToString();
+    }
+}
