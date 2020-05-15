ef0b093d40085de930397079741e5957832f9076
yah01 <kagaminehuan@gmail.com>
2020-05-15 20:35:51 +0800

    将FrequencyAnalyst类单独分出来

---
+++ FrequencyAnalyst.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CipherBreaker
+{
+	static class FrequencyAnalyst
+	{
+		private static Dictionary<string, double> frequencyDict = new Dictionary<string, double>();
+
+		public static double Analyze(string str)
+		{
+			return 0.0;
+		}
+
+		public static string[] Split(string str)
+		{
+			return null;
+		}
+
+	}
+}
