163dd255442509822f984d6d49431715865515cc
Photooon <1527842029@qq.com>
2020-05-21 17:12:14 +0800

    add web crawler

---
+++ WordFrequencyCrawler/WordFrequencyCrawler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using WebCrawler;
+
+namespace WordFrequencyCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Crawler crawler = new Crawler("http://www.chinadaily.com.cn", 2, 25, "test.txt");
+            crawler.Start();
+        }
+    }
+}
