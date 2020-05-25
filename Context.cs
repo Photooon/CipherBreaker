50e85bc8eb457f3b5e770a907ce26908921ac25f
yah01 <kagaminehuan@gmail.com>
2020-05-25 12:44:09 +0800

    Create Context.cs

---
+++ Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Markup;
+
+namespace CipherBreaker
+{
+	class Context
+	{
+		private Scheme scheme;
+		public double BestProb;
+		public string BestKey;
+		public Dictionary<string, object> ctx;
+
+		Context(Scheme scheme)
+		{
+			this.scheme = scheme;
+		}
+	}
+}
