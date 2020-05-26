00611525211ae53468ad097ca6884c8f13441dc6
yah01 <kagaminehuan@gmail.com>
2020-05-26 21:21:04 +0800

    添加CommonData静态类

---
+++ CommonData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace CipherBreaker
+{
+	static class CommonData
+	{
+		public static ObservableCollection<Task> tasks = new ObservableCollection<Task>();
+	}
+}
