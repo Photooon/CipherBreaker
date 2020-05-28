cf66e20c54009589462e2df7cf9661e101239188
yah01 <kagaminehuan@gmail.com>
2020-05-28 21:50:43 +0800

    Add Context

--- Context.cs
+++ Context.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Windows.Markup;
 
 namespace CipherBreaker
@@ -10,11 +12,34 @@ namespace CipherBreaker
 		private Scheme scheme;
 		public double BestProb;
 		public string BestKey;
-		public Dictionary<string, object> Ctx;
+		private Dictionary<string, object> ctx;
 
 		Context(Scheme scheme)
 		{
 			this.scheme = scheme;
 		}
+
+		public object Get(string key)
+		{
+			return ctx.GetValueOrDefault(key);
+		}
+
+		public void Set(string key,object value)
+		{
+			ctx[key] = value;
+		}
 	}
+
+	//class ContextJsonConvert : JsonConverter<Context>
+	//{
+	//	public override Context Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	//	{
+			
+	//	}
+
+	//	public override void Write(Utf8JsonWriter writer, Context value, JsonSerializerOptions options)
+	//	{
+	//		writer.WriteString("");
+	//	}
+	//}
 }
