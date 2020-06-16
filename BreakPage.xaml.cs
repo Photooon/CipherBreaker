ebb44703e90b3baef5bf281bfcfecc435dd44994
lyy1240056777 <61934334+lyy1240056777@users.noreply.github.com>
2020-06-16 11:58:07 +0800

    前段修改
    任务列表中的任务可通过开始键进行操作 新建的任务还无法加入页面 还要设计破解的可视化 暂停破解 删除任务 判断明文和密钥合法性 这些功能

---
+++ BreakPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CipherBreaker
+{
+    /// <summary>
+    /// BreakPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class BreakPage : Page
+    {
+        public BreakPage(Task task)
+        {
+            InitializeComponent();
+            this.task = task;
+            this.scheme = Scheme.NewScheme(task.type, task.OriginText, task.ResultText, task.Key);
+        }
+
+        private Task task;
+        private Scheme scheme;
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            (Result.Text, _) = scheme.Break(scheme.Plain);
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,28 +28,34 @@ namespace CipherBreaker
 			debugWindow.Show();
 			this.TaskListBox.ItemsSource = CommonData.Tasks;
 			Task task1 = new Task();
-			task1.Name = "first";
+			task1.Name = "first";                                    //测试用
+			task1.OptType = OperationType.Decode;					 //测试用
+			task1.type = SchemeType.Caesar;							 //测试用
+			task1.Key = "3";										 //测试用
+			task1.ResultText = "dddtttttttttttttttttttttttttttttt";	 //测试用
+			task1.Date = DateTime.Now;								 //测试用
 			CommonData.Tasks.Add(task1);
+
 			Task task2 = new Task();
-			task2.Name = "second";
+			task2.Name = "second";                                                 //测试用
+			task2.OptType = OperationType.Encode;								   //测试用
+			task2.type = SchemeType.Caesar;										   //测试用
+			task2.Key = "5";													   //测试用
+			task2.OriginText = "iiiiiiiiiiiizzzzzzzzzzzzzoooooooonnnnnnnnneeeeee"; //测试用
+			task2.Date = DateTime.Now;											   //测试用
 			CommonData.Tasks.Add(task2);
+
+			Task task3 = new Task();
+			task3.Name = "third";                                       //测试用
+			task3.OptType = OperationType.Break;						//测试用
+			task3.OriginText = "jmpwfzpv";  	//测试用
+			task3.Date = DateTime.Now;									//测试用
+			CommonData.Tasks.Add(task3);
 		}
 		private void NewTaskButton_Click(object sender, RoutedEventArgs e)
 		{
 			NewTaskWindow newTaskWindow = new NewTaskWindow();
 			newTaskWindow.Show();
-		}
-
-		private void GuideButton_Click(object sender, RoutedEventArgs e)
-		{
-			GuideWindow guideWindow = new GuideWindow();
-			guideWindow.Show();
-		}
-
-		private void AlgorithmButton_Click(object sender, RoutedEventArgs e)
-		{
-			AlgorithmWindow algorithmWindow = new AlgorithmWindow();
-			algorithmWindow.Show();
 		}
 
 		private void OptionButton_Click(object sender, RoutedEventArgs e)
@@ -60,12 +66,36 @@ namespace CipherBreaker
 
 		private void TaskListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
-			//DetailButton.Visibility = Visibility.Visible;
-			//DetailButton.IsEnabled = true;
+			Task task = TaskListBox.SelectedItem as Task;
+			if(task.OptType==OperationType.Encode)
+			{
+				EncodePage encodePage = new EncodePage(task);
+				ContentControl.Content = new Frame() { Content = encodePage };
+				encodePage.TaskTitle.Content += task.ToString();
+				encodePage.SchemeType.Content += task.type.ToString();
+				encodePage.Key.Content += task.Key;
+				encodePage.Text.Text += "\n" + task.OriginText;
+				encodePage.Date.Text += "\n" + task.Date.ToString();
+			}
+			else if(task.OptType==OperationType.Decode)
+			{
+				DecodePage decodePage = new DecodePage(task);
+				ContentControl.Content = new Frame() { Content = decodePage };
+				decodePage.TaskTitle.Content += task.ToString();
+				decodePage.SchemeType.Content += task.type.ToString();
+				decodePage.Key.Content += task.Key;
+				decodePage.Text.Text += "\n" + task.ResultText;
+				decodePage.Date.Text += "\n" + task.Date.ToString();
+			}
+			else if(task.OptType==OperationType.Break)
+			{
+				BreakPage breakPage = new BreakPage(task);
+				ContentControl.Content = new Frame() { Content = breakPage };
+				breakPage.TaskTitle.Content += task.ToString();
+				breakPage.Text.Text += "\n" + task.OriginText;
+				breakPage.Date.Text += "\n" + task.Date.ToString();
+			}
 		}
 
-		private void DetailButton_Click(object sender, RoutedEventArgs e)
-		{
-		}
 	}
 }
