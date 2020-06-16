903a5f7d1dc151072870998ab1caf455c4a59023
yah01 <kagaminehuan@gmail.com>
2020-06-16 15:56:25 +0800

    删除废弃代码

--- NewTaskWindow.xaml.cs
+++ NewTaskWindow.xaml.cs
@@ -57,56 +57,23 @@ namespace CipherBreaker
             task.type = GetSchemeTypeBySelectedIndex(EncodeScheme.SelectedIndex);
             task.Date = DateTime.Now;
             task.Key = EncodeKey.Text;
-             
-            EncodePage encodePage = new EncodePage(task);
-            encodePage.TaskTitle.Content += task.ToString();
-            encodePage.SchemeType.Content += task.type.ToString();
-            encodePage.Key.Content += task.Key;
-            encodePage.Text.Text += "\n" + task.OriginText;
-            encodePage.Date.Text += "\n" + task.Date.ToString();
-            //MainWindow.ContentControl.Content = new Frame() { Content = encodePage };//在主窗体中创建新页
-            CommonData.Tasks.Add(task);
-            this.mainWindow.TaskListBox.SelectedIndex = mainWindow.TaskListBox.Items.Count - 1;
+            CommonData.AddAndSelect(task, mainWindow.TaskListBox);
             this.Close();
-            
-
-
-            //OperateWindow operateWindow = new OperateWindow(task);
-
-            //operateWindow.TaskTitle.Content = "任务名："+task.Name;
-            //operateWindow.OptType.Content = "操作：" + task.OptType;
-            //operateWindow.SchemeType.Content = "算法：" + task.type.ToString();
-            //operateWindow.Key.Content = "密钥：" + task.Key;
-            //operateWindow.Text.Text = "明文：" + task.OriginText;
-            //operateWindow.Date.Content = "创建时间：" + task.Date.ToString();
-            //operateWindow.Show();
-
-            //CommonData.Tasks.Add(task);
         }
 
         private void DecodeButton_Click(object sender, RoutedEventArgs e)
         {
             Task task = new Task();
-
             task.Name = DecodeTitle.Text;
             task.ResultText = Cipher.Text;
             task.OriginText = null;
             task.OptType = OperationType.Decode;
-            if (DecodeScheme.SelectedIndex == 0) { task.type=SchemeType.Caesar;}
-            else if (DecodeScheme.SelectedIndex == 1) { task.type = SchemeType.Substitution; }
-            else if (DecodeScheme.SelectedIndex == 2) { task.type = SchemeType.RailFence; }
-            else if (DecodeScheme.SelectedIndex == 3) { task.type = SchemeType.Affine; }
+            task.type = GetSchemeTypeBySelectedIndex(DecodeScheme.SelectedIndex);
             task.Date = DateTime.Now;
             task.Key = DecodeKey.Text;
-            OperateWindow operateWindow = new OperateWindow(task);
 
-            operateWindow.TaskTitle.Content = "任务名：" + task.Name;
-            operateWindow.OptType.Content = "操作：" + task.OptType;
-            operateWindow.SchemeType.Content = "算法：" + task.type.ToString();
-            operateWindow.Key.Content = "密钥：" + task.Key;
-            operateWindow.Text.Text = "密文：" + task.OriginText;
-            operateWindow.Date.Content = "创建时间：" + task.Date.ToString();
-            operateWindow.Show();
+            CommonData.AddAndSelect(task, mainWindow.TaskListBox);
+            this.Close();
         }
 
         private void BreakButton_Click(object sender, RoutedEventArgs e)
@@ -118,18 +85,9 @@ namespace CipherBreaker
             task.ResultText = null;
             task.OptType = OperationType.Break;
             task.Date = DateTime.Now;
-            OperateWindow operateWindow = new OperateWindow(task);
 
-            operateWindow.TaskTitle.Content = "任务名：" + task.Name;
-            operateWindow.OptType.Content = "操作：" + task.OptType;
-            //operateWindow.SchemeType.Content = "算法：" + task.type.ToString();
-            //operateWindow.Key.Content = "密钥：" + task.Key;
-            //operateWindow.Text.Text = "密文：" + task.OriginText;
-            operateWindow.SchemeType.IsEnabled = false;
-            operateWindow.Key.IsEnabled = false;
-            operateWindow.Text.IsEnabled = false;
-            operateWindow.Date.Content = "创建时间：" + task.Date.ToString();
-            operateWindow.Show();
+            CommonData.AddAndSelect(task, mainWindow.TaskListBox);
+            this.Close();
         }
 
         private void KeyDefault_Click(object sender, RoutedEventArgs e)
